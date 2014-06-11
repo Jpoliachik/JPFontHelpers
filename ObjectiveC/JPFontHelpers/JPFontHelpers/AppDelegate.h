d0a21d30209c4c0a77d42db665dd9ca418a6c51e
Justin Poliachik <jpoliachik@gmail.com>
2014-06-10 23:46:20 -0400

    Initial Commit

---
+++ ObjectiveC/JPFontHelpers/JPFontHelpers/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  JPFontHelpers
+//
+//  Created by Justin Poliachik on 6/10/14.
+//  Copyright (c) 2014 justinpoliachik. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ ObjectiveC/JPFontHelpers/JPFontHelpers/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  JPFontHelpers
+//
+//  Created by Justin Poliachik on 6/10/14.
+//  Copyright (c) 2014 justinpoliachik. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
