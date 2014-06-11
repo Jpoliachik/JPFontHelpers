0a9a0e3538600823e754f3e9073cf7e328e6deb3
Justin Poliachik <jpoliachik@gmail.com>
2014-06-10 23:56:12 -0400

    Created JPFontHelper Categories

---
+++ ObjectiveC/JPFontHelpers/JPFontHelpers/Classes/UIFont+JPFontHelpers.h
@@ -0,0 +1,13 @@
+//
+//  UIFont+JPFontHelpers.h
+//  JPFontHelpers
+//
+//  Created by Justin Poliachik on 6/10/14.
+//  Copyright (c) 2014 justinpoliachik. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface UIFont (JPFontHelpers)
+
+@end
---
+++ ObjectiveC/JPFontHelpers/JPFontHelpers/Classes/UIFont+JPFontHelpers.m
@@ -0,0 +1,13 @@
+//
+//  UIFont+JPFontHelpers.m
+//  JPFontHelpers
+//
+//  Created by Justin Poliachik on 6/10/14.
+//  Copyright (c) 2014 justinpoliachik. All rights reserved.
+//
+
+#import "UIFont+JPFontHelpers.h"
+
+@implementation UIFont (JPFontHelpers)
+
+@end
